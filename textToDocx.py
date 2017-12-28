450787f7fd0ebf7e5a8f6c49a18426edcdc177c8
eli-cobler <ecobler11@gmail.com>
2017-12-27 23:43:28 -0600

    Fixing no space issue.
    removed unneeded line of code to fix no spaces in word document bug.

--- textToDocx.py
+++ textToDocx.py
@@ -1,5 +1,5 @@
 from docx import Document
-
+import os
 
 text_file = "/Users/elicobler/tiny_dancer/BackEnd/TextFiles/12.18.18 - Monday - Hr 2 - Seg 1.txt"
 
@@ -10,12 +10,20 @@ document = Document()
 #then use to add to the document we just created
 fileContents = []
 for line in open(text_file):
-    row = line.split(' ')
-    fileContents += list(row)
+    fileContents += line
 
 #adds all the text we just created to the document as a paragraph
 paragraph = document.add_paragraph(fileContents)
 
-#saves the document with all the under the name we give it
-document.save('test.docx')
+#gets .txt file extention to replace for renaming to .docx
+text_file_ext = os.path.splitext(text_file)[1].lower()
+
+#updates path & filename for for .docx format
+new_filename = os.path.basename(text_file).replace(text_file_ext, '.docx')
+new_path = os.path.split(text_file)[0]
+new_docx_file_path = new_path + "/" + new_filename
+
+#saves the document with all the under the name of the 
+#original text file name
+document.save(new_docx_file_path)
 print("Document saved.")
